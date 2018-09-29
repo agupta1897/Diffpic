c518fde67c3691618591f3812c8c8396f0a60e32
Thien An Duong Do <32723517+ThienAnSD@users.noreply.github.com>
2018-09-29 10:08:50 -0400

    Add Collection

--- js/select2.js
+++ js/select2.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+    
+
+
     $("#git-username").select2({
         maximumSelectionLength: 1
     });
@@ -23,10 +27,56 @@ $(document).ready(function() {
                 for (i = 0; i < res.length; i++) {
                 var newOption = new Option(res[i]["name"], i, false, false);
                 repoSelect.append(newOption).trigger('change');
-                
                 }
             } 
-            });
         });
+
+    });
+
+    repoSelect.on('select2:select', function (e) {
+        var data = e.params.data;
+        console.log(data);
+        $.ajax({
+            url: "https://api.github.com/repos/" + $("#username-field").val() + "/" + data["text"] + "/commits",
+            jsonp: true,
+            method: "GET",
+            dataType: "json",
+            success: function(res) {
+                console.log(res);
+                var sha = res[0]["sha"];
+                $.ajax({
+                    url: "https://api.github.com/repos/" + $("#username-field").val() + "/" + data["text"] + "/git/trees/" + sha + "?recursive=1",
+                    jsonp: true,
+                    method: "GET",
+                    dataType: "json",
+                    success: function(res) {
+                        console.log(res);
+                        var i;
+                        var list = document.getElementById('slides');
+
+                        for (i = 0; i < res["tree"].length; i++){
+                            if (res["tree"][i]["path"].includes(".jpg") || res["tree"][i]["path"].includes(".png") || res["tree"][i]["path"].includes(".jpeg")){
+                                var li = document.createElement("a");
+                                li.setAttribute('href', '#!');
+                                li.classList.add('collection-item');
+                                var _img=document.createElement('img');
+                                _img.src="https://raw.githubusercontent.com/" + $("#username-field").val() + "/" + data["text"] + "/" + sha + "/" + res["tree"][i]["path"];
+                                _img.id = i;
+                                li.appendChild(_img);
+                                list.appendChild(li);
+                            }
+                        }
+                        console.log(list);
+                    } 
+                });
+
+                
+            } 
+        });    
+    });
+
+    
+
+
 });
 
