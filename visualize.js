4873502cbf7565e74222dc2833e2c0cd49388287
David <damccoy1@crimson.ua.edu>
2018-09-29 15:04:50 -0400

    UPDATES

--- visualize.js
+++ visualize.js
@@ -3,21 +3,20 @@ var commitDates = new Array();
 
 function loadImagefromId(commitId1, commitId2)
 {
-    var URLName  = "http://raw.githubusercontent.com/damccoy1/picdiff/" + commitId1 + "/friends_on_a_cooler.png"
-    if(commitId2 !== null)
-    {
-   
-    $("#picture1").attr("src", URLName);
-    drawImageFromUrl(URLName, pic1, "#pic1-container");
-    $("#picture1Label").html(commitId1.substring(0,5));
-    $("#picture2Container").show();
-    $("#picture2Label").html(commitId2.substring(0,5));
-    URLName  = "http://raw.githubusercontent.com/damccoy1/picdiff/" + commitId2 + "/friends_on_a_cooler.png"
-    $("#picture2").attr("src",URLName);
-    drawImageFromUrl(URLName, pic2, "#pic2-container");
-    $("#diff-container").show();
-    $("#diff-tools").fadeIn();
-    }
+    var URLName  = "https://cors-anywhere.herokuapp.com/http://raw.githubusercontent.com/damccoy1/picdiff/" + commitId1 + "/friends_on_a_cooler.png"
+    if(commitId2 != null){
+        $("#picture1").attr("src", URLName);
+        drawImageFromUrl(URLName, pic1, "#pic1-container");
+        $("#picture1Label").html(commitId1.substring(0,5));
+        $("#picture2Container").show();
+        $("#picture2Label").html(commitId2.substring(0,5));
+        URLName  = "https://cors-anywhere.herokuapp.com/http://raw.githubusercontent.com/damccoy1/picdiff/" + commitId2 + "/friends_on_a_cooler.png"
+        $("#picture2").attr("src",URLName);
+        drawImageFromUrl(URLName, pic2, "#pic2-container");
+        $("#diff-container").show();
+        $("#diff-og-container").show();
+        $("#diff-tools").show();
+        }
     else
     {
         $("#picture1").attr("src", URLName);
@@ -25,7 +24,8 @@ function loadImagefromId(commitId1, commitId2)
         $("#picture1Label").html(commitId1.substring(0,5));
         $("#picture2Container").hide();
         $("#diff-container").hide();
-        $("#diff-tools").fadeOut();
+        $("#diff-og-container").hide();
+        $("#diff-tools").hide();
     }
     }
 
@@ -39,21 +39,21 @@ function loadImagefromId(commitId1, commitId2)
 var img1Loaded = false;
 var img2Loaded = false;
 var pic1Data, pic2Data;
+var img1Width, img2Width, img1Height, img2Height;
 
 function updateImages() {
     img1Loaded = false;
     img2Loaded = false;
-    if (($('#slider').val())>0 && ($('#slider').val())<commitIds.length)
+    if (($('#slider').val()) > 0 && ($('#slider').val()) < commitIds.length)
     {
     loadImagefromId(commitIds[$('#slider').val()-1],commitIds[$('#slider').val()])
     }
-    else if($('#slider').val() == commitIds.length)
-    {
-        loadImagefromId(commitIds[$('#slider').val()-1],null)
-    }
-    else
-    {
-        loadImagefromId(commitIds[$('#slider').val()],null)
+    else {
+        if ($("#slider").val() == 0)
+            loadImagefromId(commitIds[$('#slider').val()],null)
+        else {
+            loadImagefromId(commitIds[$('#slider').val() - 1],null)
+        }
     }
 }
 
@@ -61,44 +61,64 @@ function drawImageFromUrl(url, pic, container){
     img = new Image();
     img.onload = function() {
         URL.revokeObjectURL(this.src);
-        var picWidth = img.width;
-        var picHeight = img.height;
+        var picWidth = this.width;
+        var picHeight = this.height;
         var scale = 1;
         var yOffset = 0;
         var xOffset = 0;
-        if (img.width > img.height){
-            if (img.width > pic.width){
-                scale = pic.width / img.width;
+        if (this.width > this.height){
+            if (this.width > pic.width){
+                scale = pic.width / this.width;
             }
             else {
-                scale = img.width / pic.width;
+                scale = this.width / pic.width;
             }
-            yOffset = Math.round((pic.height - Math.round(img.height * scale)) / 2);
+            yOffset = Math.round((pic.height - Math.round(this.height * scale)) / 2);
         }
         else {
-            if (img.height > pic.height)
-            scale = pic.height / img.height;
+            if (this.height > pic.height)
+            scale = pic.height / this.height;
             else {
-                scale = img.height / pic.height;
+                scale = this.height / pic.height;
             }
-            xOffset = Math.round((pic.width - Math.round(img.width * scale)) / 2);
+            xOffset = Math.round((pic.width - Math.round(this.width * scale)) / 2);
         }
-        pic.getContext("2d").drawImage(this, 0, 0, img.width, img.height, xOffset, yOffset, Math.round(picWidth * scale), Math.round(picHeight * scale));
         if (container === "#pic1-container") {
+            pic.getContext("2d").clearRect(0, 0, pic.width, pic.height);
+            pic.getContext("2d").drawImage(this, 0, 0, this.width, this.height, xOffset, yOffset, Math.round(picWidth * scale), Math.round(picHeight * scale));
             img1Loaded = true;
+            img1Height = this.height;
+            img1Width = this.width;
             pic1Data = pic1.getContext("2d").getImageData(0, 0, pic1.width, pic1.height);
         }
         else {
+            pic.getContext("2d").clearRect(0, 0, pic.width, pic.height);
+            pic.getContext("2d").drawImage(this, 0, 0, this.width, this.height, xOffset, yOffset, Math.round(picWidth * scale), Math.round(picHeight * scale));
             img2Loaded = true;
+            img2Height = this.height;
+            img2Width = this.width;
             pic2Data = pic2.getContext("2d").getImageData(0, 0, pic2.width, pic2.height);
         }
         if (img1Loaded && img2Loaded) {
-            var rgbSelection = hexToRgb($("#diff-color").val());
-            picWidth = pic1.width;
-            picHeight = pic1.height;
-            scale = 1;
-            drawDiff(rgbSelection.red, rgbSelection.green, rgbSelection.blue, scale); 
-            diffBack.getContext("2d").drawImage(pic1, 0, 0, pic1.width, pic1.height, 0, 0, Math.round(picWidth * scale), Math.round(picHeight * scale));        
+            if (img1Height == img2Height && img1Width == img2Width){
+                var rgbSelection = hexToRgb($("#diff-color").val());
+                picWidth = pic1.width;
+                picHeight = pic1.height;
+                scale = 1;
+                drawDiff(rgbSelection.red, rgbSelection.green, rgbSelection.blue, scale); 
+                diffBack.getContext("2d").drawImage(pic1, 0, 0, pic1.width, pic1.height, 0, 0, Math.round(picWidth * scale), Math.round(picHeight * scale));            
+            }
+            else {
+                $("#diff-container").hide();
+                $("#diff-og-container").hide();
+                $("#diff-tools").hide();
+                if (img1Height == img2Width && img1Width == img2Height) {
+                    $("#rotated-alert").fadeIn();
+                }
+                else {
+                    $("#resized-alert").fadeIn();
+                }
+            }
         }
     }
     img.src = url;
@@ -157,8 +177,12 @@ $(document).ready(function(){
         $("#slider").show();
     }
     $("#slider").hide();
+    $("#rotated-alert").hide();
+    $("#resized-alert").hide();
     loadAllCommits();
-    $('#slider').on('input', function(){
+    $('#slider').on('change', function(){  
+        $("#rotated-alert").fadeOut();
+        $("#resized-alert").fadeOut();
         console.log($('#slider').val());
         updateImages();
         });
