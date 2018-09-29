34428927cab043af9b065ca004e93818c98442e2
Amber Gupta <33590295+agupta1897@users.noreply.github.com>
2018-09-29 05:35:25 -0400

    Functionality to view slider and 2 images together!

---
+++ visualize.js
@@ -0,0 +1,65 @@
+var commitIds = new Array();
+function loadFileFromGitHub()
+{
+    $("#picture").attr("src", "http://raw.githubusercontent.com/agupta1897/ImageDiffTesting/6f96e99df0f4b7c714c8f3425ee4aba1a839f23c/bitmoji-20180926060308.png");
+}
+
+function loadAllCommits()
+{
+    var URLName  = "https://api.github.com/repos/agupta1897/ImageDiffTesting/commits?path=bitmoji-20180926060308.png"
+    $.get(URLName, function(data){
+    
+        commitIds = [];
+        for( i = data.length-1; i > -1; i--)
+        {
+           commitIds.push(data[i]["sha"]);
+           $("#timeline").append("<div class='col' style='text-align:center'>" + data[i]["sha"].substring(0, 5) + "</div>");
+        }
+        $("#slider").attr("max", data.length);
+        $("#slider").attr("value", data.length/2);   
+    });
+    $("#slider").show();
+}
+
+function loadImagefromId(commitId, commitId2)
+{
+    if(commitId2 !== null)
+    {
+    var URLName  = "http://raw.githubusercontent.com/agupta1897/ImageDiffTesting/" + commitId + "/bitmoji-20180926060308.png"
+    $("#picture").attr("src", URLName);
+        $("#picture2").show()
+        URLName  = "http://raw.githubusercontent.com/agupta1897/ImageDiffTesting/" + commitId2 + "/bitmoji-20180926060308.png"
+        $("#picture2").attr("src",URLName);
+    }
+    else
+    {
+        $("#picture2").hide()
+    }
+    }
+
+// function loadImageforIds(commitIds)
+// {
+//    for( i = 0; i < commitIds.length; i++)
+//    {
+//        loadImagefromId(commitIds[i]);
+//    }
+// }
+
+$(document).ready(function(){
+    $("#slider").hide();
+    $("#get-file-btn").click(function(){
+        loadFileFromGitHub();
+        loadAllCommits();
+    });
+    $('#slider').on('input', function(){
+        console.log($('#slider').val());
+        if (($('#slider').val())>0 && ($('#slider').val())<commitIds.length)
+        {
+        loadImagefromId(commitIds[$('#slider').val()-1],commitIds[$('#slider').val()])
+        }
+        else
+        {
+            loadImagefromId(commitIds[$('#slider').val()],null)
+        }
+        });
+})
