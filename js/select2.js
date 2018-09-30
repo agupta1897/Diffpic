f5e237e1b7594d403737d9bb67b17c98a7d71b1b
Thien An Duong Do <32723517+ThienAnSD@users.noreply.github.com>
2018-09-30 00:50:45 -0400

    a

--- js/select2.js
+++ js/select2.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
     url = window.location.href;
     redirect_uri="https://damccoy1.github.io/diffpic/";
-    console.log("test linksdsd");
+    console.log("test lind");
     console.log(window.location.href);
     if (url.includes("code")) {
         code = url.substring(url.length-20);
